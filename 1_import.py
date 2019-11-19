17ae4e9b0ee0b3a4c51e1da912c099f47c398bdf
grand-coder <37803705+grand-coder@users.noreply.github.com>
2019-11-19 11:26:36 +0800

    Add files via upload

--- 1_import.py
+++ 1_import.py
@@ -1,13 +1,15 @@
 import jieba
 jieba.set_dictionary("dict.txt.big")
 jieba.load_userdict("entertain.txt")
-import jieba.analyse
+from jieba.analyse import extract_tags
+import glob
 
-# r: 讀取 w:寫入 a:寫入(結尾)
-f = open("news/a.txt", "r", encoding="utf-8")
-article = f.read()
-f.close()
+for fn in glob.glob("news/*.txt"):
+    # r: 讀取 w:寫入 a:寫入(結尾)
+    f = open(fn, "r", encoding="utf-8")
+    article = f.read()
+    f.close()
 
-print("分詞:", list(jieba.cut(article)))
-keywords = jieba.analyse.extract_tags(article)
-print("關鍵詞:", keywords)
+    print("分詞:", list(jieba.cut(article)))
+    keywords = extract_tags(article)
+    print("關鍵詞:", keywords)
